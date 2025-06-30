851646fc0f07a57c9ed138c5f745dd43f70e1e4f
Richard <olella93@gmail.com>
2025-06-30 19:15:22 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D32F6F',
+        charcoal: '#333333',
+      },
+    },
+  },
+  plugins: [],
+}
